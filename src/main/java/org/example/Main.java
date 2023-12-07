676a566f1040a80de8b3119c34cb8179877d96f3
xproce <badr.hirchoua@gmail.com>
2023-12-07 23:19:05 +0100

    Version after pushing the code to the remot repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Version 1");
+        System.out.printf("Version 1 after pushing the code to GitHub");
     }
 }
