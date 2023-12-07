00cd95259eda7c6961f2c96d1e96e788a5ef1415
xproce <badr.hirchoua@gmail.com>
2023-12-08 00:12:34 +0100

    Version 1 of the cloned repo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Version 1 after pushing the code to GitHub");
+        System.out.printf("Version 1 of the cloned repo");
     }
 }
